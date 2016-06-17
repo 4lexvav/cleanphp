e14a2a8cfc31aa88b8340a9b13b6a10527483c62
vav <l.e.h.a.vav@gmail.com>
2016-06-17 21:49:38 +0300

    Skeleton of domain layer for app

--- specs/domain/service/invoice-factory.spec.php
+++ specs/domain/service/invoice-factory.spec.php
@@ -51,15 +51,33 @@ describe('InvoicingService', function() {
 			$this->repository = $this->getProphet()->prophesize(
 				'CleanPhp\Invoicer\Domain\Repository\OrderRepositoryInterface'
 			);
+			$this->factory = $this->getProphet()->prophesize(
+				'CleanPhp\Invoicer\Domain\Factory\InvoiceFactory'
+			);
 		});
 
 		it('Should query the repository for uninvoiced Orders', function() {
+			$this->repository->getUninvoicedOrders()->willReturn([]);
+			$this->factory->createFromOrder([])->willReturn([]);
+
 			$this->repository->getUninvoicedOrders()->shouldBeCalled();
-			$service = new InvoicingService($this->repository->reveal());
+			$service = new InvoicingService($this->repository->reveal(), $this->factory->reveal());
 			$service->generateInvoices();
 		});
 
-		it('Should return an Invoice for each uninvoiced Order');
+		it('Should return an Invoice for each uninvoiced Order', function() {
+			$orders = [(new Order())->setTotal(400)];
+			$invoices = [(new Invoice())->setTotal(400)];
+
+			$this->repository->getUninvoicedOrders()->willReturn($orders);
+			$this->factory->createFromOrder($orders[0])->willReturn($invoices[0]);
+
+			$service = new InvoicingService($this->repository->reveal(), $this->factory->reveal());
+			$results = $service->generateInvoices();
+
+			expect($results)->to->be->a('array');
+			expect($results)->to->have->length(count($orders));
+		});
 
 		afterEach(function() {
 			$this->getProphet()->checkPredictions();
--- src/Domain/Service/InvoicingService.php
+++ src/Domain/Service/InvoicingService.php
@@ -3,16 +3,35 @@
 namespace CleanPhp\Invoicer\Domain\Service;
 
 use CleanPhp\Invoicer\Domain\Repository\OrderRepositoryInterface;
+use CleanPhp\Invoicer\Domain\Factory\InvoiceFactory;
 
 class InvoicingService
 {
-	public function __constrcut(OrderRepositoryInterface $orderRepository)
+	/**
+	 * @var OrderRepositoryInterface
+	 */
+	private $orderRepository;
+
+	/**
+	 * @var InvoiceFactory
+	 */
+	private $invoiceFactory;
+
+	public function __construct(OrderRepositoryInterface $orderRepository, InvoiceFactory $invoiceFactory)
 	{
 		$this->orderRepository = $orderRepository;
+		$this->invoiceFactory = $invoiceFactory;
 	}
 
 	public function generateInvoices()
 	{
+		$invoices = [];
 		$orders = $this->orderRepository->getUninvoicedOrders();
+
+		foreach ($orders as $order) {
+			$invoices[] = $this->invoiceFactory->createFromOrder($order);
+		}
+
+		return $invoices;
 	}
 }
