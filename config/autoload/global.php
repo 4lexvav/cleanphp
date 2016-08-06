4cf1d7e2d7aaaadfbb5a88bfb4cf0d82768ca119
vav <4lexvav@gmail.com>
2016-08-06 16:14:01 +0000

    Configure ZF2 database and factories

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -1,16 +1,60 @@
 <?php
-/**
- * Global Configuration Override
- *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
- *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
- */
 
-return array(
-    // ...
-);
+use CleanPhp\Invoicer\Domain\Entity\Customer;
+use CleanPhp\Invoicer\Domain\Entity\Order;
+use CleanPhp\Invoicer\Domain\Entity\Invoice;
+use CleanPhp\Invoicer\Persistence\Zend\DataTable\CustomerTable;
+use CleanPhp\Invoicer\Persistence\Zend\DataTable\OrderTable;
+use CleanPhp\Invoicer\Persistence\Zend\DataTable\InvoiceTable;
+use CleanPhp\Invoicer\Persistence\Zend\TableGateway\TableGatewayFactory;
+use Zend\StdLib\Hydrator\ClassMethods;
+
+return [
+    'service_manager' => [
+        'factories' => [
+            'CustomerTable' => function($sm) {
+                $factory = new TableGatewayFactyory();
+                $hydrator = new ClassMethods();
+
+                return new CustomerTable(
+                    $factory->createGateway(
+                        $sm->get('Zend\Db\Adapter\Adapter'),
+                        $hydrator,
+                        new Customer(),
+                        'customers'
+                    ),
+                    $hydrator
+                );
+            },
+            'OrderTable' => function($sm) {
+                $factory = new TableGatewayFactyory();
+                $hydrator = new ClassMethods();
+
+                return new OrderTable(
+                    $factory->createGateway(
+                        $sm->get('Zend\Db\Adapter\Adapter'),
+                        $hydrator,
+                        new Order(),
+                        'orders'
+                    ),
+                    $hydrator
+                );
+            },
+            'InvoiceTable' => function($sm) {
+                $factory = new TableGatewayFactyory();
+                $hydrator = new ClassMethods();
+
+                return new InvoiceTable(
+                    $factory->createGateway(
+                        $sm->get('Zend\Db\Adapter\Adapter'),
+                        $hydrator,
+                        new Invoice(),
+                        'invoices'
+                    ),
+                    $hydrator
+                );
+            },
+            'Zebd\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ],
+    ],
+];
